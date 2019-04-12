a77629dfcefffd56954dbaaf33eb9dc9df92c748
Sascha Weidner <=>
2019-04-12 15:27:49 +0200

    Prepare kernel

--- src/Kernel/Plugin.php
+++ src/Kernel/Plugin.php
@@ -4,6 +4,19 @@ namespace Sioweb\Oxid\Api\Kernel;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+use Symfony\Component\Config\Loader\LoaderResolverInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
+
 class Plugin extends Bundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function getRouteCollection(LoaderResolverInterface $resolver, KernelInterface $kernel)
+    {
+        return $resolver
+            ->resolve(__DIR__ . '/../Resources/config/routing.yml')
+            ->load(__DIR__ . '/../Resources/config/routing.yml')
+        ;
+    }
 }
