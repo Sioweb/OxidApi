f547b06bd064df94be150cce0713fe39d9717a79
Sascha Weidner <=>
2019-04-12 20:12:13 +0200

    first working prototype

---
+++ src/SiowebOxidApiBundle.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Sioweb\Oxid\Api;
+
+use Sioweb\Oxid\Api\Extension\Extension;
+use Symfony\Component\HttpKernel\Bundle\Bundle AS BaseBundle;
+use Sioweb\Oxid\Kernel\Bundle\BundleRoutesInterface;
+use Symfony\Component\Config\Loader\LoaderResolverInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+/**
+ * Configures the Contao Glossar bundle.
+ *
+ * @author Sascha Weidner <https://www.sioweb.de>
+ */
+class SiowebOxidApiBundle extends BaseBundle implements BundleRoutesInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getContainerExtension()
+    {
+        return new Extension();
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function getRouteCollection(LoaderResolverInterface $resolver, KernelInterface $kernel)
+    {
+        return $resolver
+            ->resolve(__DIR__ . '/Resources/config/routing.yml')
+            ->load(__DIR__ . '/Resources/config/routing.yml')
+        ;
+    }
+    
+}
